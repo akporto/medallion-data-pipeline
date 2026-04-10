466c82187da8c75ddfd4a24837b9d3cc39468796
akporto <akporto@users.noreply.github.com>
2026-04-10 17:40:00 -0300

    style: terraform fmt iam module

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -36,9 +36,9 @@ data "aws_iam_policy_document" "lambda_validator_policy" {
   }
 
   statement {
-    sid     = "ReadFromBronzeBucket"
-    effect  = "Allow"
-    actions = ["s3:GetObject"]
+    sid       = "ReadFromBronzeBucket"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
     resources = ["${var.bronze_bucket_arn}/raw/*"]
   }
 
